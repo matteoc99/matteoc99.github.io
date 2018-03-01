94be3a0d5de401475328013d9457b094e4ad2883
matteoc99 <matteo.cosi@live.it>
2018-03-01 12:47:32 +0100

    fix

--- js/main_script.js
+++ js/main_script.js
@@ -20,13 +20,12 @@ function sende() {
 $('a[href*="#"]')
 // Remove links that don't actually link to anything
     .not('[href="#"]')
-    .not('[href="#0"]')
     .click(function (event) {
         // On-page links
         if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
             &&
-            location.hostname == this.hostname
+            location.hostname === this.hostname
         ) {
             // Figure out element to scroll to
             var target = $(this.hash);
